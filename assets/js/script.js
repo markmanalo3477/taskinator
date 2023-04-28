29481e63af8a0de4a182b1b7b4e35ffbf2a2bd05
mark manalo <m.e.manalo77@gmail.com>
2023-04-28 19:35:53 -0400

    added form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,54 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
 
-    event.preventDefault();
+var taskFormHandler = function(event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  }
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+
+  // check if input values are empty strings
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  // clear the form input boxes after you click Add Task
+  formEl.reset();
+};
+
+var createTaskEl = function(taskDataObj) {
+  // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
+
 }
 
-buttonEl.addEventListener("click", createTaskHandler); 
+formEl.addEventListener("submit", taskFormHandler); 
   var listItemEl = document.createElement("li"); //create a new task item
   listItemEl.className = "task-item"; //style the new task item
-  listItemEl.textContent = "This is a new task."; //add the text
+  listItemEl.textContent = taskNameInput; //add the text
   tasksToDoEl.appendChild(listItemEl); //append the element to the task list
 
-formEl.addEventListener("submit", createTaskHandler);
+
